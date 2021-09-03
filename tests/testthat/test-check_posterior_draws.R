3abec9f79b7f8e42933bd4148b39c4f9b8e34b75
Måns Magnusson <mons.magnusson@gmail.com>
2021-09-03 23:07:09 +0200

    check posterior draws

---
+++ tests/testthat/test-check_posterior_draws.R
@@ -0,0 +1,25 @@
+context("test-check_posterior_draws")
+
+test_that("test-check_posterior_draws", {
+
+  expect_silent(pdb_test <- pdb_local())
+
+  expect_silent(rp <- reference_posterior_draws(x = "eight_schools-eight_schools_noncentered", pdb_test))
+  expect_silent(rp1 <- check_reference_posterior_draws(x = rp))
+  expect_silent(rp2 <- check_reference_posterior_draws(x = "eight_schools-eight_schools_noncentered", pdb = pdb_test))
+  po <- pdb_posterior("eight_schools-eight_schools_noncentered", pdb_test)
+  expect_silent(rp3 <- check_reference_posterior_draws(x = po))
+
+  expect_equal(rp1, rp2)
+  expect_equal(rp1, rp3)
+})
+
+test_that("check_pdb_posterior works", {
+
+  expect_silent(pdb_test <- pdb_local())
+
+  po <- pdb_posterior("eight_schools-eight_schools_noncentered", pdb_test)
+
+  if(on_github_actions()) skip_on_os("linux") # Currently problem with stringi on ubuntu (2021-03-10)
+  expect_message(check_pdb_posterior(po, run_stan_code_checks = FALSE), regexp = "Posterior is ok.")
+})
