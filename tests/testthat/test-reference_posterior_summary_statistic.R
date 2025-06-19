acfa9bac90af1d0071eabcad4aa3955592e9dbfe
JTorgander <55882444+JTorgander@users.noreply.github.com>
2025-06-19 18:54:19 +0200

    changing summary stats to new naming convention

--- tests/testthat/test-reference_posterior_summary_statistic.R
+++ tests/testthat/test-reference_posterior_summary_statistic.R
@@ -4,14 +4,14 @@ test_that("Check that reference_posterior_summary_statistics work as expected", 
 
   expect_silent(pdb_test <- pdb_local())
   expect_silent(po <- posterior("eight_schools-eight_schools_noncentered", pdb_test))
-  expect_silent(rpssi1 <- reference_posterior_info(po, type = "mean"))
+  expect_silent(rpssi1 <- reference_posterior_info(po, type = "mean_value"))
   expect_s3_class(rpssi1, "pdb_reference_posterior_info")
-  expect_silent(rpssi3 <- reference_posterior_info("eight_schools-eight_schools_noncentered", pdb_test, type = "mean"))
+  expect_silent(rpssi3 <- reference_posterior_info("eight_schools-eight_schools_noncentered", pdb_test, type = "mean_value"))
   expect_identical(rpssi1, rpssi3)
 
   expect_output(print(rpssi1), "Posterior: eight_schools-eight_schools_noncentered")
 
-  expect_silent(rpm1 <- reference_posterior_summary_statistic(x = po, type = "mean"))
+  expect_silent(rpm1 <- reference_posterior_summary_statistic(x = po, type = "mean_value"))
   expect_silent(rpm2 <- reference_posterior_summary_statistics(x = po))
   expect_identical(rpm1, rpm2$mean)
 
@@ -19,7 +19,7 @@ test_that("Check that reference_posterior_summary_statistics work as expected", 
 
   skip("LDA model will work from version 0.4")
   expect_silent(po <- posterior("prideprejustice_chapter-ldaK5", pdb_test))
-  expect_error(rpm <- reference_posterior_summary_statistic(po, type = "mean"),
+  expect_error(rpm <- reference_posterior_summary_statistic(po, type = "mean_value"),
                regexp = "There is currently no reference posterior for this posterior.")
 })
 
@@ -34,7 +34,7 @@ test_that("compute and write summary_statistics", {
   expect_silent(rpd <- reference_posterior_draws(po))
 
   expect_silent(rpdc <- check_summary_statistics_draws(x = rpd))
-  expect_silent(rpm <- compute_reference_posterior_summary_statistic(rpdc, "mean"))
+  expect_silent(rpm <- compute_reference_posterior_summary_statistic(rpdc, "mean_value"))
   expect_silent(rpmi <- info(rpm))
   expect_silent(rps <- compute_reference_posterior_summary_statistic(rpdc, "sd"))
   expect_silent(rpsi <- info(rps))
@@ -44,7 +44,7 @@ test_that("compute and write summary_statistics", {
   # since it uses posterior() to access the test reference posterior
   po$name <- "test_data-test_model"
   po$reference_posterior_name <- po$name
-  # remove_pdb(po, pdb_test)
+  #remove_pdb(po, pdb_test)
   write_pdb(po, pdb_test)
 
   # Test write gsd
